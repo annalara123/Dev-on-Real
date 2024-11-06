aded22ddc67381d2e3a65962ceb1e5bbcd97f6d1
Gustavo-Gadelha <gustavofg2112@gmail.com>
2024-11-06 11:52:02 -0300

    Alterações no Card #5

--- src/card5/Main.java
+++ src/card5/Main.java
@@ -2,22 +2,18 @@ package card5;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numero = new int[]{12, 31, 23, 26, 43, 23, 50, 13, 12};
+        int[] numbers = new int[]{12, 31, 23, 26, 43, 23, 50, 13, 12};
 
-        if (numero.length > 50 || numero.length == 0) {
-            throw new IllegalArgumentException("A lista de números deve conter entre 1 e 50 números");
-        }
+        assert numbers.length >= 1 && numbers.length <= 50;
 
-        for (int i : numero) {
-            if (i > 50) {
-                throw new IllegalArgumentException("");
-            }
-
-            if (i % 3 == 2) {
-                System.out.println("Adicione 1 a " + i);
-            } else {
-                System.out.println("Subtraia 1 de " + i);
+        int operations = 0;
+        for (int value : numbers) {
+            assert value >= 1 && value <= 50;
+            if (value % 3 > 0) {
+                operations++;
             }
         }
+
+        System.out.println("Número mínimo de operações: " + operations);
     }
 }
