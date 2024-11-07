162ea637f7f67f94230f0716ac09a935969cbf87
annalara123 <112590453+annalara123@users.noreply.github.com>
2024-11-07 15:15:59 -0300

    Implementando o resto do card24

--- card24/src/Main.java
+++ card24/src/Main.java
@@ -1,5 +1,3 @@
-import java.util.logging.Logger;
-
 public class Main {
     public static void main(String[] args) {
         int n = 10;
@@ -10,14 +8,22 @@ public class Main {
         }
 
         int[] sumNumbers = addIndivisibleAndDivisibleNumbersInARange(n, m);
+        int calculateDifference = sumNumbers [0] - sumNumbers[1];
+
+        System.out.println(calculateDifference);
     }
 
     public static int[] addIndivisibleAndDivisibleNumbersInARange(int finalRangeNumber, int divisionNumber) {
         int sumIndivisibleNumbers = 0;
+        int sumDivisibleNumbers = 0;
 
         for (int i = 1; i <= finalRangeNumber; i++) {
             if (i % divisionNumber != 0)
                 sumIndivisibleNumbers += i;
+            else
+                sumDivisibleNumbers += i;
         }
+
+        return new int[]{sumIndivisibleNumbers, sumDivisibleNumbers};
     }
 }
